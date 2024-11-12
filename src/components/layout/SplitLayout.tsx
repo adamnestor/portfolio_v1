4f14e55447dd5282cde8d710cfe4efdfd01f69d8
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-11-12 15:53:06 -0500

    Create Split Layout Component

---
+++ src/components/layout/SplitLayout.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+interface SplitLayoutProps {
+  leftPanel: React.ReactNode;
+  rightPanel: React.ReactNode;
+}
+
+const SplitLayout: React.FC<SplitLayoutProps> = ({ leftPanel, rightPanel }) => {
+  return (
+    <div className="flex flex-col md:flex-row min-h-screen bg-brand-light">
+      {/* Fixed Left Panel */}
+      <div className="w-full md:w-1/2 md:fixed md:h-screen p-6">
+        <div className="max-w-md mx-auto h-full flex flex-col justify-between">
+          {leftPanel}
+        </div>
+      </div>
+
+      {/* Right Panel with offset */}
+      <div className="w-full md:w-1/2 md:ml-[50%] p-6">
+        <div className="max-w-3xl mx-auto">
+          {rightPanel}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SplitLayout;
