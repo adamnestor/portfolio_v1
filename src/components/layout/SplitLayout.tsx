e6fb36ab1da0e0cd4e6d4ba46304d5a75bc61a53
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-11-13 16:25:00 -0500

    Fixed gradient issue

--- src/components/layout/SplitLayout.tsx
+++ src/components/layout/SplitLayout.tsx
@@ -7,7 +7,7 @@ const SplitLayout: React.FC<SplitLayoutProps> = ({ leftPanel, rightPanel }) => {
   return (
     <div className="flex flex-col md:flex-row min-h-screen bg-background">
       {/* Fixed Left Panel */}
-      <div className="w-full md:w-1/2 md:fixed md:h-screen bg-card-gradient p-6">
+      <div className="w-full md:w-1/2 md:fixed md:h-screen bg-background p-6">
         <div className="max-w-md mx-auto h-full flex flex-col justify-between">
           {leftPanel}
         </div>
@@ -15,9 +15,7 @@ const SplitLayout: React.FC<SplitLayoutProps> = ({ leftPanel, rightPanel }) => {
 
       {/* Right Panel with offset */}
       <div className="w-full md:w-1/2 md:ml-[50%] bg-background p-6">
-        <div className="max-w-3xl mx-auto">
-          {rightPanel}
-        </div>
+        <div className="max-w-3xl mx-auto">{rightPanel}</div>
       </div>
     </div>
   );
