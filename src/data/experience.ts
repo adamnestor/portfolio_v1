424067411aa593844f15547af87498cb1a34a72d
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-12-09 17:52:24 -0500

    Update experience.ts

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -9,25 +9,36 @@ export interface Experience {
 
 export const experiences: Experience[] = [
   {
-    title: "Software Developer",
-    company: "Your Company",
-    companyUrl: "https://company-website.com",
+    title: "Full-Stack Developer",
+    company: "We Make Good Software",
+    companyUrl: "https://wemakegoodsoftware.com/",
     dateRange: "2023 — Present",
     description: `Here you can write a detailed description of your role and achievements. 
       You can include multiple lines and be specific about your contributions and responsibilities.`,
-    technologies: ["React", "TypeScript", "Tailwind CSS"],
+    technologies: [
+      "Java",
+      "Spring Boot",
+      "React",
+      "TypeScript",
+      "RESTful APIs",
+      "Axios",
+      "Ngrok",
+    ],
   },
   {
     title: "High School Teacher",
-    company: "School Name",
-    companyUrl: "https://school-website.com",
-    dateRange: "2018 — 2023",
-    description: `Description of your teaching experience, focusing on transferable skills 
-      and achievements that relate to development and problem-solving.`,
+    company: "Lake Catholic High School",
+    companyUrl: "https://www.lakecatholic.org",
+    dateRange: "2021 — Present",
+    description: `I designed and implemented engaging lessons for theology courses and a philosophy elective, consistently translating abstract concepts into accessible learning experiences. My experience in the classroom and leading class retreats showcased my ability to manage complex projects with multiple stakeholders - including students, parents, and faculty - while adapting to changing needs`,
     technologies: [
       "Curriculum Development",
-      "Project Management",
+      "Program Management",
+      "Complex Problem Solving",
+      "Research & Analysis",
       "Communication",
+      "Cross-functional Collaboration",
+      "Adaptative Teaching",
     ],
   },
   {
