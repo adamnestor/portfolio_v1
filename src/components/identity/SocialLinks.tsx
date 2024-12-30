7520e3790d764af3fe3a4ec605946d7df7a01d94
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-12-30 15:05:43 -0500

    replaced social links with icon links

--- src/components/identity/SocialLinks.tsx
+++ src/components/identity/SocialLinks.tsx
@@ -1,19 +1,35 @@
 import { SocialLink } from "../../data/profile";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 interface SocialLinksProps {
   links: SocialLink[];
 }
 
 const SocialLinks = ({ links }: SocialLinksProps) => {
+  const getIcon = (platform: string) => {
+    switch (platform) {
+      case "GitHub":
+        return <FontAwesomeIcon icon={faGithub} size="3x" />;
+      case "LinkedIn":
+        return <FontAwesomeIcon icon={faLinkedin} size="3x" />;
+      default:
+        return platform;
+    }
+  };
+
   return (
     <div className="flex gap-6">
       {links.map(({ platform, url }) => (
         <a
           key={platform}
           href={url}
-          className="text-slate-medium text-lg hover:text-orange-primary"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-slate-medium hover:text-orange-primary"
+          aria-label={platform}
         >
-          {platform}
+          {getIcon(platform)}
         </a>
       ))}
     </div>
