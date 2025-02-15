0307d73936ddcf0d276a979ebf53908c41de6bd5
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2025-02-15 17:45:42 -0500

    update formatting

--- src/components/sections/experience/ExperienceCard.tsx
+++ src/components/sections/experience/ExperienceCard.tsx
@@ -20,40 +20,45 @@ const ExperienceCard = ({
 }: ExperienceCardProps) => {
   return (
     <div className="block group hover:bg-orange-light rounded-lg p-4 -m-4 transition-colors shadow-card hover:shadow-card-hover">
-      <div className="grid grid-cols-[140px_1fr] gap-4 pr-8">
-        <div className="space-y-2">
-          {positions.map(({ dateRange }) => (
-            <span
-              key={dateRange}
-              className="text-slate-medium text-base whitespace-nowrap"
-            >
-              {dateRange}
-            </span>
-          ))}
-        </div>
-        <div>
-          <h3 className="text-slate-dark text-lg font-medium mb-1 group-hover:text-orange-primary">
-            {title}
-          </h3>
-          <div className="space-y-2 mb-3">
-            {positions.map(({ company }) => (
-              <div key={company} className="text-slate-dark text-base">
-                {company}
-              </div>
+      <div>
+        <h3 className="text-slate-dark text-lg font-medium mb-3 group-hover:text-orange-primary">
+          {title}
+        </h3>
+        <div className="grid grid-cols-[140px_1fr] gap-x-4">
+          <div className="space-y-2">
+            {positions.map(({ dateRange }) => (
+              <span
+                key={dateRange}
+                className="text-slate-medium text-base whitespace-nowrap block"
+              >
+                {dateRange}
+              </span>
             ))}
           </div>
-          {description && (
-            <p className="text-slate-dark text-base group-hover:text-slate-dark mb-4">
-              {description}
-            </p>
-          )}
-          {technologies.length > 0 && (
-            <div className="flex flex-wrap gap-2">
-              {technologies.map((tech) => (
-                <Tag key={tech} label={tech} />
+          <div className="space-y-4">
+            <div className="space-y-2">
+              {positions.map(({ company, dateRange }) => (
+                <span
+                  key={`${company}-${dateRange}`}
+                  className="text-slate-dark text-lg font-medium block"
+                >
+                  {company}
+                </span>
               ))}
             </div>
-          )}
+            {description && (
+              <p className="text-slate-dark text-base group-hover:text-slate-dark">
+                {description}
+              </p>
+            )}
+            {technologies.length > 0 && (
+              <div className="flex flex-wrap gap-2">
+                {technologies.map((tech) => (
+                  <Tag key={tech} label={tech} />
+                ))}
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
